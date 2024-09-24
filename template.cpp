6e687379376b1a2297825617e86dc06cdb66fe4e
keinerRitter <michaelstanislavov@gmail.com>
2024-09-24 17:57:53 +0300

    Add files via upload
    My base template to solve problems

---
+++ template.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+void solution(){
+	 cout << "Hello World!" << endl;
+	}
+
+
+int main()
+{
+	int t;
+	cin >> t;
+	while(t--){
+		solution();
+		}
+	return 0;
+}
+
